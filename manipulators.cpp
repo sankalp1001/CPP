c72fd8247afd7658a6ab0ae7929431e697f8ae26
Sankalp Mallappa <75899963+sankalp1001@users.noreply.github.com>
2021-10-10 12:25:09 +0530

    Create manipulators.cpp

---
+++ manipulators.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+# include <iomanip>
+
+using namespace std;
+int main()
+{
+    int a=4,b=5,c=5,output ; 
+
+    output = a*b+c;
+    cout<< output<<endl;
+    // int a = 32;
+    // cout << "setting width " << setw(1)<<a<<endl;
+} // namespace std;
+ 
