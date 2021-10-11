3e8864f7076ba79891dfd137a5dd6dff48a3ed23
Sankalp Mallappa <75899963+sankalp1001@users.noreply.github.com>
2021-10-11 16:28:22 +0530

    Create staticMembers.cpp

---
+++ staticMembers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class employee{
+static int count;
+int id;
+public:
+ void setData(void){
+   cout << "Enter the id:"<<endl;
+   cin >> id;
+   count ++;
+ }
+ void getData(void){
+  cout << "The id is "<< id<<endl; 
+ }
+ static void Count(void){
+cout<<"count is "<<count<<endl; 
+
+ }
+};
+int employee :: count ;
+
+ int main(){
+  employee sankalp;
+  sankalp.setData();
+  sankalp.getData();
+  sankalp.Count();
+   return 0;  
+ }
+
+
+
